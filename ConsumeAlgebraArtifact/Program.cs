57fd91035923625319fb1be338c5a6b8a4cd1ba9
Narendra <Narendra@DESKTOP-C03GSGC>
2022-12-01 07:40:27 -0500

    Add project files.

---
+++ ConsumeAlgebraArtifact/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsumeAlgebraArtifact
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
